c704e8ee860ed7ee9b541c34898f6d68aeaf6d41
Martin Arroyo <marroyo7775@gmail.com>
2019-11-05 22:56:24 -0500

    Added Word Repository object

---
+++ app/src/main/java/com/martinarroyo/roomwordsample/WordRepository.java
@@ -0,0 +1,43 @@
+package com.martinarroyo.roomwordsample;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordRepository {
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    public WordRepository(Application application){
+        // Get a handle to database and initialize member variables
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAlphabetizedWords();
+    }
+
+    LiveData<List<Word>> getmAllWords(){
+        return mAllWords;
+    }
+
+    public void insert(Word word){
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
