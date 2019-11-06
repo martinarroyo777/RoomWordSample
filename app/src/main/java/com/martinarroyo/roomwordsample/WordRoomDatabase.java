fb29c4bc3fb901a043dec7329b4bf38e40c4d4b8
Martin Arroyo <marroyo7775@gmail.com>
2019-11-06 09:10:11 -0500

    Added code to populate the database when the app is started up

--- app/src/main/java/com/martinarroyo/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/martinarroyo/roomwordsample/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package com.martinarroyo.roomwordsample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Word.class},version = 1)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -20,10 +23,38 @@ public abstract class WordRoomDatabase extends RoomDatabase {
             synchronized (WordRoomDatabase.class){
                 if(INSTANCE == null){
                     // Create database here
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),WordRoomDatabase.class,"word_database").build();
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),WordRoomDatabase.class,"word_database").addCallback(sRoomDatabaseCallback).build();
                 }
             }
         }
         return INSTANCE;
+    }
+    // Create a database callback method to populate the database when we start the app
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+            Word word = new Word("Hello");
+            mDao.insert(word);
+            word = new Word("World");
+            mDao.insert(word);
+            return null;
+        }
     }
 }
